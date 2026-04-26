758140591b01a38ec7f12a95019e17c1213736ea
Prithvi Bondili <prithviramsingh@gmail.com>
2026-04-26 18:07:57 -0500

    Update macoptimizer.rb

--- Casks/macoptimizer.rb
+++ Casks/macoptimizer.rb
@@ -1,4 +1,4 @@
-   cask "macoptimizer" do
+cask "macoptimizer" do
       version "1.0.7"
       sha256 "7276e18ab6ae5e90d37ab95b766b4b48ba2793a55a266f409e21123b9257ccef"
   
@@ -13,4 +13,4 @@
        "~/Library/Caches/com.prithvibondili.macoptimizer",
        "~/Library/Preferences/com.prithvibondili.macoptimizer.plist",
      ]
-   end
+end

8bd2c82b76b3f6f4f50e3e137dd634573526594e
prithviramsingh <prithviramsingh@gmail.com>
2026-04-26 20:56:28 -0500

    chore: bump macoptimizer to 1.1.7

--- Casks/macoptimizer.rb
+++ Casks/macoptimizer.rb
@@ -1,6 +1,6 @@
 cask "macoptimizer" do
-      version "1.0.7"
-      sha256 "7276e18ab6ae5e90d37ab95b766b4b48ba2793a55a266f409e21123b9257ccef"
+      version "1.1.7"
+      sha256 "a5a5027f235a1860bc9b7072104ecc1fbd4a8e5c501b1821067867555118a0c7"
   
       url "https://github.com/prithviramsingh/MacOptimizer/releases/download/v#{version}/MacOptimizer-#{version}.dmg"
       name "MacOptimizer"
